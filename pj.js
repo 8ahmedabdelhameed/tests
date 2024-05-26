7dfe0cf396036e85e9377cf5650fb84cc7c25b11
8ahmedabdelhameed <152407322+8ahmedabdelhameed@users.noreply.github.com>
2024-05-26 12:02:01 +0300

    Add files via upload

---
+++ pj.js
@@ -0,0 +1,20 @@
+function myFunction() {
+    var x = document.getElementById("slider");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
+  }
+function camel()
+{
+document.getElementById('audio_play1').play(); return false;
+}
+function cat()
+{
+document.getElementById('audio_play').play(); return false;
+}
+function chicken()
+{
+document.getElementById('audio_play2').play(); return false;
+}
